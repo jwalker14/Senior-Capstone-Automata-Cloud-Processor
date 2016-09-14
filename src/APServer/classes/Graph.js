60c670f9eaff9cc1bb573204ef6bf9e9e017361e
Jason Walker <jwalker14@me.com>
2016-09-13 20:05:05 -0400

    Adding some needed data structures

--- src/APServer/classes/Graph.js
+++ src/APServer/classes/Graph.js
@@ -9,4 +9,48 @@ Graph.prototype.toString = function(){
 	return "DONE"
 }
 
+Graph.prototype.addVertex = function(V){
+
+}
+
+Graph.prototype.removeVertex = function(){
+
+}
+
+Graph.prototype.hasVertex = function(){
+	
+}
+
+Graph.prototype.addEdge = function(){
+
+}
+
+Graph.prototype.removeEdge = function(){
+
+}
+
+Graph.prototype.hasEdge = function(){
+
+}
+
+Graph.prototype.makeGraph = function(){
+
+}
+
+Graph.prototype.makeVertex = function(){
+
+}
+
+Graph.prototype.makeEdge = function(){
+
+}
+
+Graph.prototype.getEdges = function(){
+
+}
+
+Graph.prototype.getNeighbors = function(){
+
+}
+
 module.exports = Graph
